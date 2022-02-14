c143f090fc0d66efc42049f02fbabad6fcdf7fe5
ArgjentaCaka <ac52328@ubt-uni.net>
2022-02-14 16:58:37 +0100

    website.php

--- index.php
+++ index.php
@@ -1,27 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <title>Document</title>
-    <link rel="stylesheet" type="text/css" href="web.css"
-</head>
-<body>
-    <div class="container">
-    <div class="header">
-        <img src="logo.jpg">
-        <div class="navbar">
-         <a href="#">Home</a>
-         <a href="#">Contact Us</a>
-         <a href="#">About Us</a>
-         <a href="#">Services</a>
-        </div>
-       <button class="btn"><a href="
-        file:///C:/Users/PULSE%20Electronics/Desktop/Work/detyreshtepie.html">LogIn</a></button>
-    </div>
-        <div class="content">
-            <h1>Sample website</h1>
-            <button class="btn"><a href="">Explore</a></button>
-            <button class="btn"><a href="https://www.youtube.com/watch?v=mXrvrb2zLA4&t=845s">Subscribe</a></button>
-        </div>
-    </div>
-</body>
-</html>
+<?php
+echo ""
+?>
